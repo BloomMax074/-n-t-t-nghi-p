445df7fb14909486058cc371c754c00054e5a220
Your Name <you@example.com>
2023-06-08 13:23:19 +0700

    Setup axiosClient

---
+++ front-end/src/api/axiosClient.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import queryString from 'query-string';
+
+const axiosClient = axios.create({
+    baseURL: `http://localhost:666`,
+    headers: {
+        "content-type": "application/json",
+    },
+    paramsSerializer: (params) => queryString.stringify(params),
+});
+
+// Add a request interceptor
+axiosClient.interceptors.request.use(async (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.authorization = `Bearer ${token}`;
+    }
+    return config;
+  }, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  });
+
+// Add a response interceptor
+axiosClient.interceptors.response.use( (response) => {
+    if (response && response.data) {
+        return response.data;
+    }
+    return response;
+}, (error) => {
+    throw error;
+});
+
+export default axiosClient;
+
